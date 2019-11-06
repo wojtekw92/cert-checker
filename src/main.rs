8ea2bb82f22f2deac0682d024d9ca87bdde42b7a
wojtekw92 <wwozniak92@gmail.com>
2019-11-06 22:59:45 +0100

    Logging in JSON format added

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use clap::{App, Arg};
 
 use serde::{Serialize, Deserialize};
 use serde_json::json;
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all="camelCase")]
 enum CertyficateStatus {
     Valid,
+    SoonInvalid,
     Invalid,
 }
 
@@ -20,18 +22,18 @@ struct CertyficateData {
     domain: String,
     status: CertyficateStatus,
     #[serde(skip_serializing_if="Option::is_none")]
-    expire_in: Option<u64>,
+    expire_in: Option<i32>,
     #[serde(skip_serializing_if="Option::is_none")]
-    expired_for: Option<u64>,
+    expired_for: Option<i32>,
 }
 
-fn get_u64_value(input: Option<&str>, default_value: u64 ) -> u64 {
+fn get_value<T: FromStr>(input: Option<&str>, default_value: T ) -> T {
     match input {
         Some(val) => {
-            match val.parse::<u64>() {
+            match val.parse::<T>() {
                 Ok(val) => val,
-                Err(e) => {
-                    eprintln! ("Can not parse time value, using default error: {}", e);
+                Err(_e) => {
+                    eprintln! ("Can not parse value, using `get_value`!");
                     default_value
                 }
             }
@@ -66,39 +68,41 @@ fn main() {
                 .min_values(1)
         )
         .get_matches();
-    let time: u64 = match matches.value_of("time") {
-        Some(val) => {
-            match val.parse::<u64>() {
-                Ok(val) => val,
-                Err(e) => {
-                    eprintln! ("Can not parse time value, using default error: {}", e);
-                    300
-                }
-            }
-        },
-        None => 300
-    };
+    let time: u64 = get_value(matches.value_of("time"), 300);
+    let left_warning_time: i32 = get_value(matches.value_of("left"), 5);
+    
 
     let domains: Vec<_> = match matches.values_of_lossy("domain") {
         Some(val) => val.clone(),
         None => vec![]
     };
 
-    let handle = thread::spawn(move || loop { //remove later this move
+    let handle = thread::spawn(move || loop { //remove later this "move" for now we Hacky :) 
         for domain in &domains {
             let exp = SslExpiration::from_domain_name(&domain);
             match exp {
                 Ok(expiration) => {
-                    let days_left = expiration.days();
-                    if days_left > 5 {
-                        println!("Domain \"{}\" is vaild and will be for next {} days!", domain, days_left);
-                    } else if days_left > 0 {
-                        println!("Domain \"{}\" will be invaild in {} days!", domain, days_left);
-                    } else {
-                        println!("Error domain \"{}\" is invaild for {} days!", domain, -days_left);
+                    let status = CertyficateData {
+                        domain: domain.to_string(),
+                        status: match expiration.days() {
+                            x if x > left_warning_time => CertyficateStatus::Valid,
+                            x if x > 0  => CertyficateStatus::SoonInvalid,
+                            _ => CertyficateStatus::Invalid
+                        },
+                        expire_in: match expiration.days() {
+                            x if x>0 => Some(x),
+                            _ => None
+                        },
+                        expired_for: match expiration.days() {
+                            x if x<=0 => Some(-x),
+                            _ => None
+                        }
+                    };
+                    match serde_json::to_string(&status) {
+                        Ok(x) => println!("{}", x),
+                        Err(e) => eprintln!("Error during parsing CertyficateData: {}", e)
                     }
-
-
+                    
                 },
                 Err(e) => println!("Error for domain \"{}\": {}", domain, e)
             }
